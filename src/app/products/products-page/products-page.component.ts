9da3f7bd171e340072e6b87654a549464a3e8266
Farzin mossaed <farzinmossaed@gmail.com>
2024-12-22 22:30:04 +0330

    add @ngrx/component-store and products.store.ts file and use it in products-page component

--- src/app/products/products-page/products-page.component.ts
+++ src/app/products/products-page/products-page.component.ts
@@ -2,26 +2,33 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { ProductsPageActions } from '../state/products.actions';
 import {
-  selectProducts,
   selectProductsErrorMessage,
   selectProductsLoading,
-  selectProductsShowProductCode,
   selectProductsTotal,
+  selectProductsShowProductCode,
 } from '../state/products.selectors';
+import { ProductsStore } from '../products.store';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.css'],
+  providers: [ProductsStore],
 })
 export class ProductsPageComponent {
-  products$ = this.store.select(selectProducts);
+  products$ = this.productsStore.products$;
   total$ = this.store.select(selectProductsTotal);
-  showProductCode$ = this.store.select(selectProductsShowProductCode);
   loading$ = this.store.select(selectProductsLoading);
+  showProductCode$ = this.store.select(selectProductsShowProductCode);
   errorMessage$ = this.store.select(selectProductsErrorMessage);
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, private productsStore: ProductsStore) {
+    this.store.subscribe((store) => console.log({ store }));
+  }
+
+  ngOnInit() {
+    this.productsStore.getProducts();
+  }
 
   toggleShowProductCode() {
     this.store.dispatch(ProductsPageActions.toggleShowProductCode());
